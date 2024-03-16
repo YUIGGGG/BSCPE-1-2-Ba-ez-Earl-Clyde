6e68c62aba7049923f5e1c23ac77de4d2636bd24
YUIGGGG <163645924+YUIGGGG@users.noreply.github.com>
2024-03-16 22:08:09 +0800

    Add files via upload

---
+++ OOP.py
@@ -0,0 +1,41 @@
+import tkinter as tk
+from tkinter import PhotoImage, Button, Label, ACTIVE
+from playsound import playsound
+
+user_name = input("What is your name? ")
+user_dream_job = input("What is your dream job? ")
+count = 0
+
+def click():
+    global count
+    count += 1
+    print(count)
+    if count == 1:
+        show_user_info()
+
+def show_user_info():
+    root = tk.Toplevel()
+    root.title("Personal Information")
+    root.geometry("300x150")
+
+    user_name_and_dream_job = f"Name: {user_name}\nDream Job: {user_dream_job}"
+    label = tk.Label(root, text=user_name_and_dream_job, width=300, height=150,
+                     bg="black", fg="yellow")
+    label.pack()
+
+window = tk.Tk()
+button = Button(window,
+                text="CLICK FOR USERNAME AND USER DREAM JOB",
+                command=click,
+                font=("Comic Sans", 30),
+                fg="#00FF00",
+                bg="black",
+                activeforeground="#00FF00",
+                activebackground="black",
+                state=ACTIVE)
+playsound(r'comengsound.mp3')
+button.pack()
+
+window.mainloop()
+
+
