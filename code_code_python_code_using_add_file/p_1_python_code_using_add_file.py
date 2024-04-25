428951b5d37f5f740f54aab9c2e769afd7575c5a
YUIGGGG <earlclyde197975@gmail.com>
2024-04-25 22:21:33 +0800

    P-1 with buttons

--- code_code_python_code_using_add_file/p_1_python_code_using_add_file.py
+++ code_code_python_code_using_add_file/p_1_python_code_using_add_file.py
@@ -1,19 +1,44 @@
+import tkinter as tk
 
 def even_numbers(number):
     return number % 2 == 0
 
+def read_numbers():
+    even_numbers_list = []
+    odd_numbers_list = []
+    with open("numbers.txt", "r") as the_integers:
+        for line in the_integers:
+            number = int(line.strip())
+            if even_numbers(number):
+                even_numbers_list.append(number)
+            else:
+                odd_numbers_list.append(number)
+    return even_numbers_list, odd_numbers_list
 
-with open("numbers.txt", "r") as the_integers:
-    with open("even.txt", "w") as even_output:
-        with open("odd.txt", "w") as odd_output:
-            for line in the_integers:
-                number = int(line.strip())
-                if even_numbers(number):
-                    print("The even numbers are", number)
-                    even_output_numbers = number
-                    if number:
-                        even_output.write("Even\n")
-                else:
-                    print("The odd numbers are", number)
-                    if number:
-                        odd_output.write("Odd\n")
+def display_even_numbers():
+    even_numbers_list, _ = read_numbers()
+    even_str = "\n".join(map(str, even_numbers_list))
+    even_label.config(text=even_str)
+
+def display_odd_numbers():
+    _, odd_numbers_list = read_numbers()
+    odd_str = "\n".join(map(str, odd_numbers_list))
+    odd_label.config(text=odd_str)
+
+window = tk.Tk()
+window.title("Even and Odd Numbers")
+window.geometry("400x300")
+
+even_button = tk.Button(window, text="Even Numbers", command=display_even_numbers)
+even_button.pack(pady=10)
+
+even_label = tk.Label(window, text="", bg="lightblue", width=30, height=10)
+even_label.pack(pady=5)
+
+odd_button = tk.Button(window, text="Odd Numbers", command=display_odd_numbers)
+odd_button.pack(pady=10)
+
+odd_label = tk.Label(window, text="", bg="lightpink", width=30, height=10)
+odd_label.pack(pady=5)
+
+window.mainloop()
